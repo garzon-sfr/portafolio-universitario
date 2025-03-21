5670394729c1413aeaad97b450dcec48fc2841ec
Garzon <agarzon050@gmail.com>
2025-03-21 17:50:09 -0500

    algoritmos_nav

---
+++ js/algoritmos.js
@@ -0,0 +1,24 @@
+// Filtros tipo tabs
+const filterBtns = document.querySelectorAll(".filter-item");
+const sections = document.querySelectorAll(".filter-content");
+
+filterBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        // Quitar clase active a todos
+        filterBtns.forEach(b => b.classList.remove("active"));
+        btn.classList.add("active");
+
+        const filter = btn.getAttribute("data-filter");
+
+        // Mostrar/Ocultar secciones
+        sections.forEach(section => {
+            if (section.id === filter) {
+                section.classList.remove("hidden");
+                section.classList.add("show");
+            } else {
+                section.classList.remove("show");
+                section.classList.add("hidden");
+            }
+        });
+    });
+});
